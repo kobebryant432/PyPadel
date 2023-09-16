e814c70052f9107dc4b32f69492e4ec581849755
Thomas Moerman <thomas.moerman@ugent.be>
2023-09-16 12:28:11 +0200

    added point structure usage to points

--- src/pypadel/match_mechanics/point_mappings.py
+++ src/pypadel/match_mechanics/point_mappings.py
@@ -1,5 +1,20 @@
 # point_mappings.py
 
+POINT_STRUCTURE = {
+    "player": slice(0, 1),
+    "category": slice(1, 2),
+    "side": slice(2, 4),
+    "shot_type": slice(4, 5),
+    "direction": slice(5, 6),
+}
+
+FORCED_WINNER_POINT_STRUCTURE = {
+    **POINT_STRUCTURE,
+    "player2": slice(6, 7),
+    "side2": slice(7, 9),
+    "shot_type_2": slice(9, 10),
+}
+
 cat = {"f": "Forced Winner", "u": "Unforced Error", "w": "Winner"}
 side = {"fh": "Forehand", "bh": "Backhand", "hi": "High", "hd": "High defense"}
 shot = {
--- src/pypadel/match_mechanics/points.py
+++ src/pypadel/match_mechanics/points.py
@@ -1,4 +1,5 @@
-from .point_mappings import cat, side, shot, direction
+from .point_mappings import cat, side, shot, direction, POINT_STRUCTURE, FORCED_WINNER_POINT_STRUCTURE
+import random
 
 class Point:
     """Class to represent a point in a game.
@@ -35,6 +36,7 @@ class Point:
     side = side
     shot = shot
     direction = direction
+    POINT_STRUCTURE = POINT_STRUCTURE
 
     # New reverse shot dictionary
     reverse_shot = {value: key for key, value in shot.items()}
@@ -49,24 +51,24 @@ class Point:
             A string containing information about the end of the point.
         """
 
-        self.player = int(string[0])
-        self.category = Point.cat[string[1]]
-        self.side = Point.side[string[2:4]]
+        self.player = int(string[POINT_STRUCTURE['player']])
+        self.category = Point.cat[string[POINT_STRUCTURE['category']]]
+        self.side = Point.side[string[POINT_STRUCTURE['side']]]
 
         # Dubbel use of letter v -> if high v than it is a Vibora (V) else it is a volley (v)
-        if string[2:4] in ["hi", "hd"] and string[4] == "v":
+        if string[POINT_STRUCTURE['side']] in ["hi", "hd"] and string[POINT_STRUCTURE['shot_type']] == "v":
             self.shot_type = self.shot["V"]
         else:
             # If the shot type from string is not in shot dictionary
-            if string[4] not in self.shot:
-                if string[4:] in self.reverse_shot:
-                    self.shot_type = string[4:]
+            if string[POINT_STRUCTURE['shot_type']] not in self.shot:
+                if string[POINT_STRUCTURE['shot_type']:] in self.reverse_shot:
+                    self.shot_type = string[POINT_STRUCTURE['shot_type']:]
                 else:
-                    raise ValueError(f"Invalid shot type: {string[4:]}")
+                    raise ValueError(f"Invalid shot type: {string[POINT_STRUCTURE['shot_type']]}")
             else:
-                self.shot_type = self.shot[string[4]]
+                self.shot_type = self.shot[string[POINT_STRUCTURE['shot_type']]]
 
-        self.direction = Point.direction[string[5]]
+        self.direction = Point.direction[string[POINT_STRUCTURE['direction']]]
         self.raw = string
 
     def __str__(self) -> str:
@@ -80,15 +82,28 @@ class Point:
                 for side_key in Point.side.keys():
                     for shot_key in Point.shot.keys():
                         for direction_key in Point.direction.keys():
+                            # Generate additional point strings for forced winners
                             if cat_key == "f":
-                                for player2 in range(1, 5):
-                                    if player2 != player:  # Ensure the second player is not the same as the first player
-                                        for side2_key in Point.side.keys():
-                                            for shot2_key in Point.shot.keys():
-                                                valid_point_strings.add(str(player) + cat_key + side_key + shot_key + direction_key + str(player2) + side2_key + shot2_key)
+                                # Introduce a probability to limit the number of forced winners
+                                if random.random() < 0.5:
+                                    for player2 in range(1, 5):
+                                        if player2 != player:  # Ensure the second player is not the same as the first player
+                                            for side2_key in Point.side.keys():
+                                                for shot2_key in Point.shot.keys():
+                                                    valid_point_strings.add(str(player) + cat_key + side_key + shot_key + direction_key + str(player2) + side2_key + shot2_key)
                             else:
-                                valid_point_strings.add(str(player) + cat_key + side_key + shot_key + direction_key)
+                                # Generate point strings for all categories, not just forced winners
+                                valid_point_strings.add(str(player) + cat_key + side_key + shot_key + direction_key)                        
+        # Adjust the distribution of point strings in the final set
+        forced_winner_strings = [point_string for point_string in valid_point_strings if point_string[1] == "f"]
+        unforced_error_strings = [point_string for point_string in valid_point_strings if point_string[1] == "u"]
+        winner_strings = [point_string for point_string in valid_point_strings if point_string[1] == "w"]
+        random.shuffle(forced_winner_strings)
+        random.shuffle(unforced_error_strings)
+        random.shuffle(winner_strings)
+        valid_point_strings = set(forced_winner_strings[:1900] + unforced_error_strings[:1872] + winner_strings[:1872])
         return valid_point_strings
+
 
 
 class Winner(Point):
@@ -104,6 +119,9 @@ class Unforced_error(Point):
 class Forced_winner(Point):
     def __init__(self, string) -> None:
         super().__init__(string)
-        self.player2 = string[6]
-        self.side2 = string[7:9]
-        self.shot_type_2 = string[9]
+        self.player2 = int(string[FORCED_WINNER_POINT_STRUCTURE['player2']])
+        self.side2 = Point.side[string[FORCED_WINNER_POINT_STRUCTURE['side2']]]
+        self.shot_type_2 = Point.shot[string[FORCED_WINNER_POINT_STRUCTURE['shot_type_2']]]
+
+
+
